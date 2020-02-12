f31aaff69109e9a3b44edc9e73fa0a5a8ccb13a7
CUCCCUMBA <55094588+cucccumba@users.noreply.github.com>
2020-02-12 21:41:22 +0300

    olololo

--- map.h
+++ map.h
@@ -4,13 +4,11 @@
 
 struct _Map {
     void  (*destroy)(struct _Map* obj);
-    int   (*insert)(struct _Map* obj, void* key, void* value);
+    struct _Map*   (*insert)(struct _Map* obj, void* key, void* value);
     int   (*delete)(struct _Map* obj, void* key);
     int   (*change)(struct _Map* obj, void* key, void* newValue);
     void* (*get)(struct _Map* obj, void* key);
     int   (*compare_keys)(void* key1, void* key2);
-    void  (*print_key)(void* key);
-    void  (*print_value)(void* value);
     int   (*count_value)(struct _Map* obj, void* value);
 };
 typedef struct _Map Map;
